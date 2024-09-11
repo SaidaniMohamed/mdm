6a2aa8a014fb4c5a99a12132c73973ac9cc0e203
Mohamed El Amine Saidani <mohamed.saidani@univ-constantine2.dz>
2024-09-11 12:44:58 -0700

    Update mdm.sh

--- mdm.sh
+++ mdm.sh
@@ -57,17 +57,17 @@ select opt in "${options[@]}"; do
             dscl -f "$dscl_path" localhost -append "/Local/Default/Groups/admin" GroupMembership $username
 
             # Block MDM domains
-            echo "0.0.0.0        deviceenrollment.apple.com" >>/Volumes/macOS\ Base\ System/etc/hosts
-            echo "0.0.0.0        mdmenrollment.apple.com" >>/Volumes/macOS\ Base\ System/etc/hosts
-            echo "0.0.0.0        iprofiles.apple.com" >>/Volumes/macOS\ Base\ System/etc/hosts
+            echo "0.0.0.0         deviceenrollment.apple.com" >>/Volumes/macOS\ Base\ System/etc/hosts
+            echo "0.0.0.0         mdmenrollment.apple.com" >>/Volumes/macOS\ Base\ System/etc/hosts
+            echo "0.0.0.0         iprofiles.apple.com" >>/Volumes/macOS\ Base\ System/etc/hosts
             echo -e "${GRN}Successfully blocked MDM & Profile Domains"
 
             # Remove configuration profiles
             touch /Volumes/Data/private/var/db/.AppleSetupDone
-            rm -rf /Volumes/"$system_volume"/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
-            rm -rf /Volumes/"$system_volume"/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
-            touch /Volumes/"$system_volume"/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
-            touch /Volumes/"$system_volume"/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
+            rm -rf /Volumes/macOS\ Base\ System/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
+            rm -rf /Volumes/macOS\ Base\ System/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
+            touch /Volumes/macOS\ Base\ System/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
+            touch /Volumes/macOS\ Base\ System/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
 
             echo -e "${GRN}MDM enrollment has been bypassed!${NC}"
             echo -e "${NC}Exit terminal and reboot your Mac.${NC}"
